647b129e72beaee2f8f61650751db565a1ea45fe
Mike Drew <mike@optidatallc.com>
2016-04-08 21:51:38 -0400

    added comments

--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ namespace matrixRotate
     {
         static void Main(string[] args)
         {
+            //read from STDIN
+            //
+            //4 4 1    <--- header: 4 = ROW 4 = COL 1 = Roations
+            //1 2 3 4  <--- The matrix
+            //5 6 7 8
+            //9 10 11 12
+            //13 14 15 16
 
             string header = Console.ReadLine();
             string[] headerParts = header.Split(' ');
@@ -38,6 +45,7 @@ namespace matrixRotate
 
             int[,] outMatrix = new int[IN_ROW, IN_COL];
 
+            //the number of paths that will need to be generated
             int levels = Math.Min(IN_ROW, IN_COL) / 2;
 
             //Build paths            
@@ -46,8 +54,9 @@ namespace matrixRotate
             for (int currentLevel = 0; currentLevel < levels; currentLevel++)
             {
 
-                int rowMargin = IN_ROW - currentLevel;
+                int rowMargin = IN_ROW - currentLevel; 
 
+                //The path is built in such a way that it can be walked in a clockwise direction.
                 List<Tuple<int, int>> top = new List<Tuple<int, int>>();
                 List<Tuple<int, int>> right = new List<Tuple<int, int>>();
                 List<Tuple<int, int>> bottom = new List<Tuple<int, int>>();
@@ -88,19 +97,20 @@ namespace matrixRotate
 
                 }
 
-                //merge into one path
+                //merge top, right, bottom, and left into one path - and store it.
                 levelPaths.Add(top.Concat<Tuple<int, int>>(right).Concat<Tuple<int, int>>(bottom).Concat<Tuple<int, int>>(left).ToList<Tuple<int, int>>());
 
             }
             
             //rotate
+            //newIdx is the position that will be copied to oldIdx, for example for a rotation by 1 the value in (0,1) will be copied into (0,0)
             foreach(List<Tuple<int,int>> t in levelPaths){
 
                 int startIdx = IN_ROTATE % t.Count;
                 
                 for(int i=startIdx; i<t.Count+startIdx; i++){
 
-                    int newIdx = i;
+                    int newIdx = i; 
 
                     if (newIdx >= t.Count)
                     {
@@ -124,6 +134,7 @@ namespace matrixRotate
 
             }
 
+            //print the roated matrix
             for (int outRow = 0; outRow < IN_ROW; outRow++)
             {
                 for (int outCol = 0; outCol < IN_COL; outCol++)
